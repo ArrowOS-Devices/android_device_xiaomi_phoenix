e56e5232dfb28ba98ad1e63e289f686ed1b1ea3d
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    phoenix: Enforce RRO for all resource overlays
    This configuration will likely go in mainline_system configuration.
    
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -372,6 +372,8 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
