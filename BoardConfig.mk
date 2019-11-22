89a28df90629424dc1a26c2a878a73a1bbe1544a
Jackeagle <jackeagle102@gmail.com>
2019-11-22 16:48:27 +0100

    phoenix: Set VNDK Version
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,9 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
+# Treble
+BOARD_VNDK_VERSION := current
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
