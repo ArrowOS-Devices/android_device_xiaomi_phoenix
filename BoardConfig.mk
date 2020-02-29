af6d3e4c94ab8d5add43357466d0a9f47df2f0ed
Peter Cai <peter@typeblog.net>
2020-02-29 15:41:22 +0800

    phoenix: build our own Light HAL
    * The Light HAL in official vendor partition does not work with AOSP.
      Let's get rid of it and build our own.
    * Use vendor_overlay to override the original one.
    
    Co-Authored-By: LuK1337 <priv.luk@gmail.com>
    Co-Authored-By: Demon000 <demonsingur@gmail.com>
    Co-Authored-By: Michael Bestas <mkbestas@lineageos.org>
    Co-Authored-By: Wang Han <416810799@qq.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 TARGET_INIT_VENDOR_LIB := libinit_phoenix
 TARGET_RECOVERY_DEVICE_MODULES := libinit_phoenix
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7 buildvariant=user
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
--- device.mk
+++ device.mk
@@ -35,6 +35,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/media_profiles_vendor.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_phoenix
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
