4babdc6fd74e0c600d215a41bd5ac8b9f444bce3
Peter Cai <peter@typeblog.net>
2020-03-06 15:27:50 +0800

    phoenix: Expose aux camera to OpenCamera and Google Camera
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -13,3 +13,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Bluetooth
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth.so
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,com.google.android.GoogleCamera
+
