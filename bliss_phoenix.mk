745d32999e320138499ec2bde137e4b32cc5b38c
Jackeagle <jackeagle102@gmail.com>
2020-03-10 11:50:35 +0100

    phoenix: Make Standalone source for Phoenix
    - Since we are not using sm6150-common tree as of now, make this as standalone device source for phoenix
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_phoenix.mk
+++ bliss_phoenix.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common BlissRoms stuff.
@@ -12,7 +16,7 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bliss_phoenix
 PRODUCT_DEVICE := phoenix
-PRODUCT_BRAND := Redmi
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
 
