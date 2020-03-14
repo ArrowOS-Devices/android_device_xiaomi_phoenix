5077769d82ec6b8ae02f3814893eedd539385c21
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2020-03-15 01:08:15 +0530

    phoenix: Set shipping api level
    Change-Id: If0202fad91db7f3ad214d4d382964c31448067b6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -50,6 +50,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Product
+PRODUCT_SHIPPING_API_LEVEL := 29
+
 # Secure element
 PRODUCT_PACKAGES += \
     SecureElement
