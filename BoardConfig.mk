7db7229e3cd15ac41ac408096a4768852e8ffda7
Jackeagle <jackeagle102@gmail.com>
2020-04-23 16:51:47 +0200

    phoenix: Blacklist GCam and Dialer
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,11 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_phoenix
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# GAPPS
+TARGET_GAPPS_OVERRIDE += \
+    GoogleCamera \
+    GoogleDialer
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket loop.max_part=7 buildvariant=user
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
