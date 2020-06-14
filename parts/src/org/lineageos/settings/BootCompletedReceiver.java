ced6782ae327b4f52d64311450f1504bf21ff44d
harsh-23 <harsh.v23labs@gmail.com>
2020-06-14 21:49:30 +0530

    Revert "phoenix: parts: Remove Lineage Doze Components"
    This reverts commit 5f332c8304ca9ba147461085fa564c7744a2779a.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -43,7 +44,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             // Ignore
         }
 
+        // Doze
         DiracUtils.initialize(context);
+        DozeUtils.checkDozeService(context);
+        // Force apply our default value for doze if it is not set.
+        DozeUtils.enableDoze(context, DozeUtils.isDozeEnabled(context));
     }
 
 }
