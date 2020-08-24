0685a6bd4ce3a8a43f2a123ae8c9c99b439aac4a
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-08-25 02:28:59 +0530

    phoenix: Switch to StormBreaker Kernel
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,7 +96,7 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_CONFIG := phoenix_defconfig
+  TARGET_KERNEL_CONFIG := vendor/phoenix-perf_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/phoenix
 endif
