eeb2a5ccd6e11e0b0aa230efca2df408fe8b078f
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2020-11-10 20:59:24 +0530

    phoenix: add props to make the blur smoother

--- product_prop.mk
+++ product_prop.mk
@@ -45,7 +45,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1
+    ro.sf.blurs_are_expensive=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
 
 # Priv-app permission
 PRODUCT_PRODUCT_PROPERTIES += \
