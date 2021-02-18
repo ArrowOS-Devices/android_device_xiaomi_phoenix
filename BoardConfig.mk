feb77993c40917ed35be7ac9aa01be482a0abb34
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-02-18 15:34:28 +0100

    phoenix: import compatibility matrix from V12.1.2.0.RGHINXM
    Change-Id: I12b73205a4a295a93d05f340a7888b39b3a025ce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,7 @@ AUDIO_FEATURE_ENABLED_TFA98XX_FEEDBACK := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_phoenix
