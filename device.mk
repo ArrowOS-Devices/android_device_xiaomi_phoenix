63e8cc8b04918bac3ad6966dd369be9afb265c6f
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-02-19 18:56:48 +0100

    phoenix: copy some privapp permissions
    Change-Id: I7bc6c4d397484d0326d0ad4e8773222dd9edd1bb

--- device.mk
+++ device.mk
@@ -380,7 +380,8 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
