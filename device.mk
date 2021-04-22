3ff9b6df4f21a13b4b5a1129de644e0d29622000
Arian <arian.kulmer@web.de>
2021-04-22 22:42:22 +0530

    phoenix: Build new aidl qti power HAL

--- device.mk
+++ device.mk
@@ -398,7 +398,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.phoenix
+    android.hardware.power-service-qti
 
 # Protobuf
 PRODUCT_PACKAGES += \
