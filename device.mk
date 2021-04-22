c57ec2e0c8edb498062b13894c0b590fb4a9f2ac
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-04-22 17:11:37 +0530

    phoenix: build some camera packages
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>

--- device.mk
+++ device.mk
@@ -156,7 +156,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libxml2 \
+    GCamGOPrebuilt \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
 	libmedia_jni_shim
