e792fc3a43411b9142d7065c5b991bd3c4f1c3f1
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-04-22 17:09:38 +0530

    phoenix: build some bluetooth packages
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>

--- device.mk
+++ device.mk
@@ -144,6 +144,16 @@ PRODUCT_COPY_FILES += \
         frameworks/av/services/audiopolicy/config/stub_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/stub_audio_policy_configuration.xml \
         frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    libbluetooth_qti \
+    libbthost_if \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     GCamGOPrebuilt
