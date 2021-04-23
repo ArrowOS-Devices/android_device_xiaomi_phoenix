43aad2fa909d2338e9e902c8b69789af39d71f99
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-04-23 18:17:16 +0200

    Revert "[DNM] phoenix: go permissive"
    This reverts commit 69bbc8f28e68e471a90cf0ef8671c5a7600a7909.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,6 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1  loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 ifeq ($(TARGET_PREBUILT_KERNEL),)
