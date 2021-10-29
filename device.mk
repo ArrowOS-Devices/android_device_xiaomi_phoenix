47e9df1c55a18a11eb8d6c0ac62d9eff59e4c157
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-10-29 20:10:22 +0530

    Revert "[TEMP] phoenix: Drop IFAA"
    This reverts commit 2f15293774bdbec9be4352513d5e70d933e0cb3c.

--- device.mk
+++ device.mk
@@ -283,6 +283,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Init
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc \
