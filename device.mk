c0eb08caf5213550795b4b3f4e960765fdb9cc8a
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-11-19 21:57:26 +0530

    phoenix: Switch to AOSP WFD
    Change-Id: I92cd9f2afa5d5a6ebf59d12767c3768ce3db53ae

--- device.mk
+++ device.mk
@@ -534,11 +534,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy \
     $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/configs/seccomp/wfdservice.policy:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/seccomp_policy/wfdservice.policy \
     $(LOCAL_PATH)/configs/seccomp/vendor.qti.hardware.dsp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/vendor.qti.hardware.dsp.policy \
     $(LOCAL_PATH)/configs/seccomp/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy \
-    $(LOCAL_PATH)/configs/seccomp/wfdhdcphalservice.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/wfdhdcphalservice.policy \
-    $(LOCAL_PATH)/configs/seccomp/wfdvndservice.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/wfdvndservice.policy
 
 # Sensor
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -53,7 +53,7 @@ function blob_fixup() {
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml )
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
     ;;
-    system_ext/lib64/libwfdnative.so | vendor/lib64/libgoodixhwfingerprint.so )
+    vendor/lib64/libgoodixhwfingerprint.so )
     "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
     ;;
     system_ext/lib64/lib-imsvideocodec.so )
