eade08331a4fe4885f9a90df953f83db79a7332e
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-12-12 12:04:15 +0530

    phoenix: overlays: Swtich to RRO
    Change-Id: I1fc5cb946b3dad535500961a96784f39f663c419

--- device.mk
+++ device.mk
@@ -404,8 +404,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_phoenix/com.nxp.mifare.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+PRODUCT_PACKAGES += \
+    DialerOverlayPhoenix \
+    FrameworksResOverlayPhoenix \
+    SettingsOverlayPhoenix \
+    SettingsProvidorOverlayPhoenix \
+    SystemUIOverlayPhoenix \
+    TelephonyOverlayPhoenix \
+    TelephonyProviderOverlayPhoenix \
+    TetheringConfigOverlay \
+    WifiOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -606,8 +614,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    TetheringConfigOverlay \
-    WifiOverlay \
     libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
