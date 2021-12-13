26bb71511f2c95dbf49b50896ac2f03dd9128193
Arian <arian.kulmer@web.de>
2021-12-13 09:04:59 +0000

    phoenix: fingerprint: Clang-format and clean up the code
    Change-Id: I78ca8f67846290826bd8c75c0db794bb5f477cde
    Signed-off-by: Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>

--- fingerprint/service.cpp
+++ fingerprint/service.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,64 +16,30 @@
 
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150"
 
-#include <android/log.h>
-#include <hidl/HidlSupport.h>
 #include <hidl/HidlTransportSupport.h>
-#include <android/hardware/biometrics/fingerprint/2.1/IBiometricsFingerprint.h>
-#include <android/hardware/biometrics/fingerprint/2.1/types.h>
+
 #include "BiometricsFingerprint.h"
 
-using android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
-using android::hardware::biometrics::fingerprint::V2_1::implementation::BiometricsFingerprint;
+using android::sp;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
-using android::sp;
-
-using android::status_t;
-
-status_t BiometricsFingerprint::registerAsSystemService() {
-    status_t ret = 0;
-
-    ret = IBiometricsFingerprint::registerAsService();
-    if (ret != 0) {
-        ALOGE("Failed to register IBiometricsFingerprint (%d)", ret);
-        goto fail;
-    } else {
-        ALOGI("Successfully registered IBiometricsFingerprint");
-    }
-
-#ifdef XIAOMI_FINGERPRINTEXTENSION
-    ret = IXiaomiFingerprint::registerAsService();
-    if (ret != 0) {
-        ALOGE("Failed to register IXiaomiFingerprint (%d)", ret);
-        goto fail;
-    } else {
-        ALOGI("Successfully registered IXiaomiFingerprint");
-    }
-#endif /* XIAOMI_FINGERPRINTEXTENSION */
-
-fail:
-    return ret;
-}
+using android::hardware::biometrics::fingerprint::V2_1::IBiometricsFingerprint;
+using android::hardware::biometrics::fingerprint::V2_1::implementation::BiometricsFingerprint;
 
 int main() {
-    android::sp<BiometricsFingerprint> service = nullptr;
-
-    service = new BiometricsFingerprint();
-    if (service == nullptr) {
-        ALOGE("Instance of BiometricsFingerprint is null");
-        return 1;
-    }
+    android::sp<IBiometricsFingerprint> bio = BiometricsFingerprint::getInstance();
 
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    status_t status = service->registerAsSystemService();
-    if (status != android::OK) {
-        ALOGE("Cannot register service for Fingerprint HAL(%d).", status);
-        return 1;
+    if (bio != nullptr) {
+        if (::android::OK != bio->registerAsService()) {
+            return 1;
+        }
+    } else {
+        ALOGE("Can't create instance of BiometricsFingerprint, nullptr");
     }
 
     joinRpcThreadpool();
 
-    return 0; // should never get here
+    return 0;  // should never get here
 }
