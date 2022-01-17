8fd3f15f25c710c36331238f48103e1655463cd2
daniml3 <daniel@danielml.dev>
2022-01-17 15:55:35 +0000

    phoenix: Shim libgui's surface constructor
    Change-Id: Iae687a04204aa0802aaa8276110f31c0d49e1c82
    Signed-off-by: Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -5,5 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := lib-imsvt.cpp
 LOCAL_MODULE := lib-imsvt
 LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := libgui libutils
 
 include $(BUILD_SHARED_LIBRARY)
