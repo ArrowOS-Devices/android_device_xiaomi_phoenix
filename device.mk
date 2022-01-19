2ef1994a4589a3afb4cb261a30d330bc01ac9721
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-01-19 19:10:24 +0530

    phoenix: rootdir: Remove init.qcom.efs.sync.sh
    Change-Id: I916dc544d1876d1f782707b923bba874729dcfee

--- device.mk
+++ device.mk
@@ -490,7 +490,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
     init.qcom.sdio.sh \
     init.qcom.sensors.sh \
