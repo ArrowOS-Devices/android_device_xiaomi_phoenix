b081cd1a6b03490baeece6156501d3048aa7ca5f
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-01-19 19:23:05 +0530

    phoenix: rootdir: Remove init.qcom.usb.sh
    Change-Id: I174778d13ba20d3c9b8fcc67aeae409f27397d00

--- device.mk
+++ device.mk
@@ -492,7 +492,6 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
     init.qti.chg_policy.sh \
     init.qti.dcvs.sh \
     init.qcom.factory.rc \
