3a753d16e59af7786a0d2a69e3557d29c3963e06
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-01-19 19:12:35 +0530

    phoenix: rootdir: Remove init.qcom.sensors.sh
    Change-Id: I02eb8b8926c5a8859b948216ec3cbd25b3a573f6

--- device.mk
+++ device.mk
@@ -491,7 +491,6 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
