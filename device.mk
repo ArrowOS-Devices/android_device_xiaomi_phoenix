74cc0cf0788f36f63c746f5a9ea25f6fea13eef4
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-01-19 19:07:03 +0530

    phoenix: rootdir: Remove init.qcom.coex.sh
    Change-Id: I671ea5f4de84e1b40aa1d4fc6564096286940006

--- device.mk
+++ device.mk
@@ -489,7 +489,6 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.qcom.coex.sh \
     init.qcom.early_boot.sh \
     init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
